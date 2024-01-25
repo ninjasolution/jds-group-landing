4aab891b00fffabea115c37dcd7e75213d060db8
nikhilroy2 <nikhilchandraroypoet@gmail.com>
2024-01-25 15:58:37 +0600

    update navbar

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import "../../styles/header.scss";
 function Header(props) {
   return (
-    <header id="Header">
-        header
-      <Navbar></Navbar>
+    <header id="header" className="flex items-end">
+      <div className="container_wrapper">
+        <Navbar></Navbar>
+      </div>
     </header>
   );
 }
--- src/layout/header/Navbar.jsx
+++ src/layout/header/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-
+import React from "react";
+import PymbleRoad from "../../assets/icons/PymbleRoad.svg";
+import TeleLogo from "../../assets/icons/TeleLogo.svg";
 function Navbar(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="flex justify-between items-end w-100">
+      <img src={PymbleRoad} alt="img" />
+      <a href="#" className="inline-block h-[52px]">
+        <img src={TeleLogo} className="max-h-[95%]" alt="img" />
+      </a>
+      <ul className="flex items-center">
+        <li className="me-6">
+          <a className="text-[#7c7262]" href="#">LANGUAGES</a>
+        </li>
+        <li className="me-6">
+          <a className="text-[#7c7262]" href="#">CONTACT</a>
+        </li>
+        <li>
+          <div className="hamburger hamburger--collapse">
+            <div className="hamburger-box">
+              <div className="hamburger-inner" />
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;

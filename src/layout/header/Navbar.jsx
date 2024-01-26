6f7c73e1618474801d42579ce3778bc396d8647d
nikhilroy2 <nikhilchandraroypoet@gmail.com>
2024-01-26 14:17:12 +0600

    navbar toggle update

--- src/layout/header/Navbar.jsx
+++ src/layout/header/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import PymbleRoad from "../../assets/icons/PymbleRoad.svg";
 import TeleLogo from "../../assets/icons/TeleLogo.svg";
+import Navigation from "./Navigation";
 function Navbar(props) {
+  const [toggle, setToggle] = useState(false);
+  const hamburger_btn_ref = useRef(null);
   return (
     <div className="flex justify-between items-end w-100">
       <img src={PymbleRoad} alt="img" />
@@ -10,19 +13,35 @@ function Navbar(props) {
       </a>
       <ul className="flex items-center">
         <li className="me-6">
-          <a className="text-[#7c7262]" href="#">LANGUAGES</a>
+          <a className="text_primary" href="#">
+            LANGUAGES
+          </a>
         </li>
         <li className="me-6">
-          <a className="text-[#7c7262]" href="#">CONTACT</a>
+          <a className="text_primary hover:underline" href="#">
+            CONTACT
+          </a>
         </li>
-        <li>
-          <div className="hamburger hamburger--collapse">
-            <div className="hamburger-box">
+        <li className="text_primary min-w-[25px]">
+          <div ref={hamburger_btn_ref}
+            onClick={() => setToggle(!toggle)}
+            className={`hamburger hamburger--collapse ${
+              toggle ? "is-active" : ""
+            }`}
+          >
+            <div className="hamburger-box me-[10px]">
               <div className="hamburger-inner" />
             </div>
+            <a
+              href="javascript:void(0)"
+              className="text_primary hover:text-[#4d4e4c]"
+            >
+              {toggle ? "CLOSE" : "MENU"}
+            </a>
           </div>
         </li>
       </ul>
+      <Navigation toggle={toggle} setToggle={setToggle} hamburger_btn_ref={hamburger_btn_ref} />
     </div>
   );
 }

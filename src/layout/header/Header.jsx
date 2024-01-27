2bb7b73304586f8b0473001ee962a31665e0d87a
nikhilroy2 <nikhilchandraroypoet@gmail.com>
2024-01-27 10:00:38 +0600

    language dropdown updated

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Navbar from "./Navbar";
+import HeaderNavbar from "./Navbar";
 import "../../styles/header.scss";
 function Header(props) {
   return (
     <header id="header" className="flex items-end">
       <div className="container_wrapper !mb-4">
-        <Navbar></Navbar>
+        <HeaderNavbar></HeaderNavbar>
       </div>
     </header>
   );
--- src/layout/header/Navbar.jsx
+++ src/layout/header/Navbar.jsx
@@ -3,9 +3,64 @@ import PymbleRoad from "../../assets/icons/PymbleRoad.svg";
 import TeleLogo from "../../assets/icons/TeleLogo.svg";
 import Navigation from "./Navigation";
 import { Link } from "react-router-dom";
-function Navbar(props) {
+
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
+  Card,
+  IconButton,
+} from "@material-tailwind/react";
+import {
+  CubeTransparentIcon,
+  UserCircleIcon,
+  CodeBracketSquareIcon,
+  Square3Stack3DIcon,
+  ChevronDownIcon,
+  Cog6ToothIcon,
+  InboxArrowDownIcon,
+  LifebuoyIcon,
+  PowerIcon,
+  RocketLaunchIcon,
+  Bars2Icon,
+} from "@heroicons/react/24/solid";
+
+function HeaderNavbar(props) {
   const [toggle, setToggle] = useState(false);
   const hamburger_btn_ref = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const closeMenu = () => setIsMenuOpen(false);
+  // profile menu component
+  const languageList = [
+    {
+      label: "ES",
+      icon: UserCircleIcon,
+    },
+    {
+      label: "DE",
+      icon: Cog6ToothIcon,
+    },
+    {
+      label: "RU",
+      icon: InboxArrowDownIcon,
+    },
+    {
+      label: "ZH",
+      icon: LifebuoyIcon,
+    },
+    {
+      label: "AR",
+      icon: PowerIcon,
+    },
+  ];
+
   return (
     <div className="flex justify-between items-end w-100">
       <img src={PymbleRoad} alt="img" />
@@ -14,12 +69,53 @@ function Navbar(props) {
       </a>
       <ul className="flex items-center">
         <li className="me-6">
-          <a className="text_primary text-sm" href="#">
-            LANGUAGES
-          </a>
+          <Menu
+            open={isMenuOpen}
+            handler={setIsMenuOpen}
+            placement="bottom-end"
+          >
+            <MenuHandler>
+              <Button
+                variant="text"
+                color="blue-gray"
+                className="text_primary text-sm m-0 p-0 font-normal"
+              >
+                LANGUAGES
+              </Button>
+            </MenuHandler>
+            <MenuList className="p-1 custom_dropdown_list">
+              {languageList.map(({ label, icon }, key) => {
+                // const isLastItem = key === languageList.length - 1;
+                return (
+                  <MenuItem
+                    key={label}
+                    onClick={closeMenu}
+                    className={`flex items-center gap-2 rounded`}
+                  >
+                    {React.createElement(icon, {
+                      className: `h-4 w-4 hidden`,
+                      strokeWidth: 2,
+                    })}
+                    <Typography
+                      as="span"
+                      variant="small"
+                      className="font-normal"
+                    >
+                      {label}
+                    </Typography>
+                  </MenuItem>
+                );
+              })}
+            </MenuList>
+          </Menu>
         </li>
         <li className="me-6">
-          <Link to={'/contact'} className="text_primary text-sm hover:underline">CONTACT</Link>
+          <Link
+            to={"/contact"}
+            className="text_primary text-sm hover:underline"
+          >
+            CONTACT
+          </Link>
         </li>
         <li className="text_primary min-w-[25px]">
           <div
@@ -50,4 +146,4 @@ function Navbar(props) {
   );
 }
 
-export default Navbar;
+export default HeaderNavbar;
